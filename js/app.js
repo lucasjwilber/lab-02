dbc8e6bbc17c4b23e635a8b3e5cfa303ad541d1c
Lucas Wilber <lucasjwilber@gmail.com>
2019-10-16 10:27:56 -0700

    added constructor function, json  function, and html template function

---
+++ js/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+let allPaintings = [];
+
+function AnimalPainting(image_url, title, description, keyword, horns) {
+  this.image_url = image_url;
+  this.title = title;
+  this.description = description;
+  this.keyword = keyword;
+  this.horns = horns;
+  allPaintings.push(this);
+};
+
+
+$.get('data/page-1.json', painting => {
+  painting.forEach(painting => {
+    new AnimalPainting(painting.image_url, painting.title, painting.description, painting.keyword, painting.horns).render();
+  });
+});
+
+{/* <section id="photo-template">
+        <h2></h2>
+        <img src="" alt="">
+        <p></p>
+      </section> */}
+
+const $template = $('#photo-template');
+
+AnimalPainting.prototype.render = function () {
+
+  const $thisPaintingTemplate = $('#photo-template').html();
+
+  const $newSection = $('<section></section>');
+
+  $($newSection).html($thisPaintingTemplate);
+
+  $($newSection).find('h2').text(this.title);
+  $($newSection).find('img').attr('src', this.image_url);
+  $($newSection).find('p').text(this.description);
+  $('main').append($newSection);
+}
